499d852b22ef8700ac5bc794f0a41f751ec739b7
enigma79 <spydiii.hackiez@gmail.com>
2021-05-02 13:26:00 +0530

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/114834239

--- codeforces/263/A.cpp
+++ codeforces/263/A.cpp
@@ -1,25 +1,22 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
- 
 
-int main(){
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int x=-1,y=-1;
-    for(int i=1;i<=5;i++){
-    	for(int j=1;j<=5;j++){
-    		int a;
-    		cin>>a;
-    		if(a==1){
-    			x=i;y=j;
-    			break;
-    		}
-    	}
-    	if(x!=-1)
-    		break;
+int main() {
+    int a[5][5],t1,t2;
+    for(int i=1;i<6;i++)
+    {
+        for(int j=1;j<6;j++)
+        {
+            cin>>a[i-1][j-1];
+            if(a[i-1][j-1]==1)
+            {
+                t1=i;
+                t2=j;
+            }
+        }
     }
-    cout<<(abs(x-3))+(abs(y-3));
-    
+    cout<<abs(3-t1)+abs(3-t2);
+	// your code goes here
 	return 0;
 }
