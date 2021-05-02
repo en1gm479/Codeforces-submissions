4d5633eabcb37aeeece08f8ca82226641e9c1fae
enigma79 <spydiii.hackiez@gmail.com>
2021-05-02 21:24:00 +0530

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/114927514

--- codeforces/977/A.cpp
+++ codeforces/977/A.cpp
@@ -1,17 +1,19 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
- 
-int main(){
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-	int n,k;
-	cin>>n>>k;
-	while(k--){
-		if(n%10==0)
-			n/=10;
-		else
-			n-=1;
-	}
-	cout<<n;
+
+int main() {
+    int long long n;
+    int k;
+    cin>>n>>k;
+    while(k)
+    {
+        if(n%10==0)
+        n/=10;
+        else
+        n-=1;
+        k--;
+    }
+    cout<<n;
+	// your code goes here
 	return 0;
 }
