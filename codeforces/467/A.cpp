9771166a15a1c1733d2d8e13464a7878b5372541
enigma79 <spydiii.hackiez@gmail.com>
2021-05-06 13:14:00 +0530

    Add solution for problem `A - George and Accommodation` Link: https://codeforces.com/contest/467/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/467/submission/115392136

---
+++ codeforces/467/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int a,b,count=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        if(b-a>=2)
+        count++;
+    }
+    cout<<count;
+	// your code goes here
+	return 0;
+}
