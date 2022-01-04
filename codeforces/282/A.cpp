5c019e009593d8110819890fdc69a82d2887c60a
enigma79 <spydiii.hackiez@gmail.com>
2022-01-04 20:23:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/141653430

--- codeforces/282/A.cpp
+++ codeforces/282/A.cpp
@@ -1,29 +1,22 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
- 
-#define rof(a,b,count) for(ll i=a;i<b;i+=count)
-#define trav(a, x)  for(auto& a : x)
-#define display(x) trav(a,x) cout<<a<<" ";cout<<endl;
-#define test() int t; cin>>t; while(t--)
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define endl "\n"
-#define ll long long int
-const unsigned int mod = 1000000007;
-
 
 int main(){
-  IOS;
-  int t;
-  cin>>t;
-  int tot=0;
-  while(t--){
-    string s;
-    cin>>s;
-    if(s[1]=='+') tot++;
-    else tot--;
-  }
-  cout<<tot;
-
-  return 0;
+          int n,a=0;
+          cin>>n;
+          while(n--){
+                    char s[5];
+                    cin>>s;
+                    if(s[0]=='X' && s[1]=='+')
+                    a++;
+                    else if(s[0]=='X' && s[1]=='-')
+                    a--;
+                    else if(s[0]=='+')
+                    ++a;
+                    else if(s[0]=='-')
+                    --a;
+                    
+          }
+          cout<<a;
+          return 0;
 }
-
