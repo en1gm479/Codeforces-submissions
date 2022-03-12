88a5c1928fce39ff0bbfd1ce2f99315868116ee1
enigma79 <spydiii.hackiez@gmail.com>
2022-03-12 09:43:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/149374387

---
+++ codeforces/282/A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(ll i=a;i<b;i+=count)
+#define trav(a, x)  for(auto& a : x)
+#define display(x) trav(a,x) cout<<a<<" ";cout<<endl;
+#define test() int t; cin>>t; while(t--)
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define ll long long int
+const unsigned int mod = 1000000007;
+
+
+int main(){
+  IOS;
+  int t;
+  cin>>t;
+  int tot=0;
+  while(t--){
+    string s;
+    cin>>s;
+    if(s[1]=='+') tot++;
+    else tot--;
+  }
+  cout<<tot;
+
+  return 0;
+}
+
