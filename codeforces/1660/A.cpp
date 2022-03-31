7ac9c281256e1d30acecb67695b42cad7e731cb0
enigma79 <spydiii.hackiez@gmail.com>
2022-03-31 21:27:00 +0530

    Add solution for problem `A - Vasya and Coins` Link: https://codeforces.com/contest/1660/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1660/submission/151567844

---
+++ codeforces/1660/A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define rof(a,b,count) for(ll i=a;i<b;i+=count)
+#define trav(a, x)  for(auto& a : x)
+#define display(x) trav(a,x) cout<<a<<" ";cout<<endl;
+#define test() int t; cin>>t; while(t--)
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define ll long long int
+const unsigned int mod = 1000000007;
+
+
+int main(){
+  IOS;
+  test(){
+    int n,m;
+    cin>>n>>m;
+    if(n==0) cout<<1;
+    else{
+      cout<<(1*n+2*m)+1;
+    }
+    cout<<endl;
+
+  }
+  return 0;
+}
+
